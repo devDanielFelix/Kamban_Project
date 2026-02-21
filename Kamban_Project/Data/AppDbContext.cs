97abce80972496f5696098198b515a257e0438a0
devDanielFelix <danielfelix72@hotmail.com>
2026-02-20 23:33:51 -0300

    Add context

--- Kamban_Project/Data/AppDbContext.cs
+++ Kamban_Project/Data/AppDbContext.cs
@@ -4,5 +4,12 @@ namespace Kamban_Project.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext>Options) : base(Options)
+        {
+            
+        }
+
+
+
     }
 }
